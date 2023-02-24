ba176cd334b652b9b3f7d5d74af72f043a79d5ea
Brent Alex Götte <95861244+brentgotte@users.noreply.github.com>
2023-02-24 10:20:01 +0100

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -14,7 +14,8 @@ export default function Contact(){
         <Link href="mailto:brent.gotte@gmail.com"> <img src='/mail.png'height={60} width={60}/> </Link>
         </div>
 
-      <form action="" class="space-y-8" name='netlify form' netlify >
+      <form action="" Method="POST" class="space-y-8" name='netlify form' netlify >
+          <input type="hidden" name="form-name" value="contact">
           <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
               <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"required/>
